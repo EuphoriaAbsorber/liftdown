8083ce18d34869a18318c5f8095962a73f74be2c
kolganchik <kolganchik@gmail.com>
2021-03-19 16:53:40 +0300

    + сорт

--- lift/lift.cpp
+++ lift/lift.cpp
@@ -22,17 +22,21 @@ int main()
     set <pair<int, pair<long long, char>>> help_set;
     vector <pair<int, pair<long long, char>>> yy;
     char dir; // направление
-    //lift x;
-        cin >> lift_count >> nowtime;// флаг + время вызова
+    int request_count; // количество запросов
+
+    cin >> lift_count; // количество лифтов
+    vector <lift> lifts;
+    for (int i = 0; i < lift_count; i++) {
+        lift d;
+        lifts.push_back(d);
+    }
+    lift* liftpointer = &lifts[0];
+
+    cin >> request_count; // количество запросов
+    for (int l = 0; l < request_count; l++) {
+        cin >>  nowtime;// время вызова
         int n; // количество вызовов на этажи
-        
-        vector <lift> lifts;
-        for (int i = 0; i < lift_count; i++) {
-            lift d;
-            lifts.push_back(d);
-        }
-        lift* liftpointer = &lifts[0];
-        
+
         int a;
         cin >> n;
         for (int i = 0; i < n; i++) {
@@ -47,14 +51,57 @@ int main()
         string s;
         getline(cin, s);
         for (int i = 0; i < n; i++) {
-            
+
             getline(cin, s);
             istringstream ss(s);
             int k;
-            while(ss >> k){
+            while (ss >> k)
                 m[yy[i]].push_back(k);
+
+            if (yy[i].second.second = '^')
+                sort(m[yy[i]].begin(), m[yy[i]].end());
+            if (yy[i].second.second = 'v')
+                sort(m[yy[i]].rbegin(), m[yy[i]].rend());
+
+            /*
+            if (yy[i].second.second = '#') {
+                sort(m[yy[i]].begin(), m[yy[i]].end());
+                vector <int> less;
+                vector <int> more;
+                for (int j = 0; j < m[yy[i]].size(); j++) {
+                    if (m[yy[i]][j] < yy[i].second.first)
+                        less.push_back(m[yy[i]][j]);
+                    else
+                        more.push_back(m[yy[i]][j]);
+                }
+                int time_up = 0;
+                int time_down = 0;
+                for (int j = 1; j < less.size(); j++)
+                    time_down += 10 * abs(less[j] - less[j-1]);
+                time_down += abs(less[0] - yy[i].second.first);
+                time_down += abs(less[less.size()-1] - yy[i].second.first);
+                for (int j = 1; j < more.size(); j++)
+                    time_up += 10 * abs(more[j] - more[j - 1]);
+                time_up += abs(more[0] - yy[i].second.first);
+                time_up += abs(more[more.size() - 1] - yy[i].second.first);
+
+                if (time_down < time_up) {
+                    for (int j = 0; j < less.size(); j++)
+                        m[yy[i]][j] = less[j];
+                    for (int j = less.size(); j < less.size()+more.size(); j++)
+                        m[yy[i]][j] = more[j-less.size()];
+                }
+                else {
+                    for (int j = 0; j < more.size(); j++)
+                        m[yy[i]][j] = more[j];
+                    for (int j = more.size(); j < less.size() + more.size(); j++)
+                        m[yy[i]][j] = less[j - more.size()];
+                }
             }
+            */
+
         }
+    }
         
         while (outcalls.size()) {
 
@@ -68,11 +115,9 @@ int main()
             for (int i = 0; i < lift_count; i++)
                 if (lifts[i].is_lift_free(total_time)) {
                     for (auto z : outcalls) {
-                        //cout << 998 << endl;
                         if (z.first > lifts[i].m_time)
                             break;
                         if (z.first <= lifts[i].m_time && z.second.first == lifts[i].m_floor) {
-                            //cout << 999 << endl;
                             for (int j = 0; j < m[z].size(); j++)
                                 lifts[i].gotofloor(m[z][j]);
                             help_set.erase(z);
@@ -83,7 +128,6 @@ int main()
                 }
 
             for (auto  z : outcalls) {
-                //int r = 0;
                 int r = findlift(liftpointer,lift_count,z.second.first,total_time);
                 if (r == -1)
                     break;
@@ -94,12 +138,6 @@ int main()
                 }
             outcalls = help_set; 
 
-            //if (lifts[0].is_lift_free(total_time)) 
-            //    cout << total_time << endl;
-
-            //cout << total_time << endl;
-            
- 
             total_time += 10;
         }
     for (int i = 0; i < lift_count; i++)
